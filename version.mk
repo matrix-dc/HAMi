8335e4db73dd56d157d61408aa86f38449f16022
chendan <doke.hi@gmail.com>
2024-09-03 17:35:22 +0800

    push image into our repository

--- version.mk
+++ version.mk
@@ -4,10 +4,11 @@ CMDS=scheduler vGPUmonitor
 DEVICES=nvidia
 OUTPUT_DIR=bin
 TARGET_ARCH=amd64
+# 镜像在咱们的仓库中有 切记从docker hub拉去的时候要加入--platform=linux/amd64
 GOLANG_IMAGE=golang:1.21-bullseye
-NVIDIA_IMAGE=nvidia/cuda:12.3.2-devel-ubuntu22.04
+NVIDIA_IMAGE=nvidia/cuda:12.2.0-devel-ubuntu20.04
 DEST_DIR=/usr/local/vgpu/
 
-VERSION = v0.0.1
+VERSION = v2.3.12-20240903-c278112
 IMG_NAME =hami
 IMG_TAG="${IMG_NAME}:${VERSION}"
